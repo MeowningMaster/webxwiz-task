707cda278ef7a2472f233c958afeb8832a31cd8c
MeowningMaster <meowningmaster@gmail.com>
2023-08-07 23:33:12 +0300

    typo

--- src/services/mongo/index.ts
+++ src/services/mongo/index.ts
@@ -1,7 +1,7 @@
 import { Config } from '#root/config/index.js'
 import { ioc } from '#root/ioc/index.js'
 import { Model, createConnection } from 'mongoose'
-import * as schemas from './shemas/index.js'
+import * as schemas from './schemas/index.js'
 import { Static } from '@fastify/type-provider-typebox'
 
 type Schemas = typeof schemas
